da163412f2a520d170059e9be154caa17aee2995
Jason Abbott <dev@trailimage.com>
2014-01-05 17:34:37 -0700

    more exif normalizations

--- routes/photo-route.js
+++ routes/photo-route.js
@@ -62,6 +62,7 @@ function normalizeLens(text)
 		.replace('35.0 mm f/2.0', 'Nikkor 35mm f/2.0D')
 		.replace('150.0 mm f/2.8', 'Sigma 150mm f/2.8 EX DG HSM APO')
 		.replace('90.0 mm f/2.8', 'Tamron 90mm f/2.8 SP AF Di')
+		.replace('24.0 mm f/3.5', 'Nikkor PC-E 24mm f/3.5D ED')
 		.replace('10.0-20.0 mm f/4.0-5.6', 'Sigma 10-20mm f/4-5.6 EX DC HSM')
 		.replace('18.0-200.0 mm f/3.5-5.6', 'Nikkor 18-200mm f/3.5-5.6G ED VR');
 }
