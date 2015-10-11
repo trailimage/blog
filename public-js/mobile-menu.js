ddc3e6c27c89b11872641036cfde202cd2137d22
Jason Abbott <dev@trailimage.com>
2015-10-10 19:40:07 -0600

    mobile menu layout

--- public-js/mobile-menu.js
+++ public-js/mobile-menu.js
@@ -4,15 +4,23 @@ $(function() {
 	var css = 'selected';
 	var $button = $('#mobile-menu-button');
 	var $menu = $('#mobile-menu');
+	var prepared = false;
+	var visible = false;
 
 	$button.click(function() {
-		$menu.show(function() {
-			$menu.find('.close').one('click', function() {
-				$menu.hide();
-			});
-		});
+		if (visible) {
+			$menu.hide(0, function() { visible = false; });
+		} else {
+			$menu.show(0, prepare);
+		}
 	});
 
-	//console.log($button);
+	function prepare() {
+		visible = true;
+		if (prepared) { return; }
 
+		$menu.find('.close').click(function() { $menu.hide(0, function() { visible = false; }); });
+
+		prepared = true;
+	}
 });
