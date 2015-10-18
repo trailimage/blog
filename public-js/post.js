4ff9aae35aa04cfec88ecfe2f9dcfb6938896684
Jason Abbott <dev@trailimage.com>
2015-10-18 10:08:43 -0600

    mobile photo info

--- public-js/post.js
+++ public-js/post.js
@@ -5,14 +5,42 @@ $(function() {
 	var $photos = $('.photo');
 
 	$photos.find('img').lazyload();
-	$photos.find('.exif').on('mouseenter click', function() {
-		var $exif = $(this);
-		// EXIF DIV has a data-url property for loading details
-		$exif.off('mouseenter click')
-			.addClass('loading')
-			.html('<span class="glyphicon glyphicon-download"></span><p>Loading …</p>')
-			.load($exif.data('url'), function() {
-				$exif.removeClass('loading').addClass('loaded');
-			});
+	$photos.find('.mobile-button').on('click', function() {
+		showExif.call(this);
 	});
+	$photos.find('.exif-button').on('mouseover', function() {
+		showExif.call(this, true);
+
+		var $exif = $(this);
+
+		$exif.parent().append($('<div>')
+			.addClass('exif')
+			.html('<span class="glyphicon glyphicon-download"></span><p>Loading …</p>')
+			.load($exif.data('url'))
+		);
+		// EXIF DIV has a data-url property for loading details
+		//$exif.off('mouseenter click')
+		//	.addClass('loading')
+		//	.html('<span class="glyphicon glyphicon-download"></span><p>Loading …</p>')
+		//	.load($exif.data('url'), function() {
+		//		$exif.removeClass('loading').addClass('loaded');
+		//	});
+	});
+
+	/**
+	 * @param {Boolean} [removeButton] Whether to remove button after showing EXIF
+	 */
+	function showExif(removeButton) {
+		var $button = $(this);
+		var $photo = $button.parent();
+		var url = $photo.data('exif');
+
+		$exif.parent().append($('<div>')
+				.addClass('exif')
+				.html('<span class="glyphicon glyphicon-download"></span><p>Loading …</p>')
+				.load($exif.data('url'))
+		);
+
+
+	}
 });
