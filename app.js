6cdcef8afcc9389b52cae26638ad50a4445f6791
Jason Abbott <dev@trailimage.com>
2015-10-31 16:16:45 -0600

    logging provider

--- app.js
+++ app.js
@@ -1,76 +1,72 @@
 'use strict';
 
 /**
+ * Application entry point
  * @see http://code.google.com/apis/console/?pli=1#project:1033232213688:access
  */
 const config = require('./lib/config.js');
-const template = require('./lib/template.js');
 const Express = require('express');
-const log = require('./lib/log.js');
 
-// middleware
-const compress = require('compression');
-const bodyParser = require('body-parser');
-const cookies = require('cookies');
-const wwwhisper = require('connect-wwwhisper');
+injectDependencies();
+createWebService();
 
-const RedisProvider = require('./lib/providers/redis-cache.js');
-const FlickrProvider = require('./lib/providers/flickr-db.js');
-
-config.provider.cache = new RedisProvider(config.env('REDISCLOUD_URL'));
-config.provider.library = new FlickrProvider({
-	key: env('FLICKR_KEY'),
-	userID: '60950751@N04',
-	appID: '72157631007435048',
-	secret: env('FLICKR_SECRET'),
-	token: env('FLICKR_TOKEN'),
-	tokenSecret: env('FLICKR_TOKEN_SECRET'),
-	defaultCollection: '72157630885395608',
-	photoSet: {
-		featured: '72157631638576162',
-		poetry: '72157632729508554'
-	}
-});
-
-
-// these depend on the redis settings
-const outputCache = require('./lib/output-cache.js');
-
-/**
- * @type {ExpressHbs}
- * @see https://github.com/donpark/hbs/blob/master/examples/extend/app.js
- * @see https://npmjs.org/package/express-hbs
- */
-const hbs = require('express-hbs');
-const app = Express();
-/** @type {Number} */
-const port = process.env['PORT'] || 3000;
-
-configure();
-
-//- Private methods -----------------------------------------------------------
-
-function configure() {
+function createWebService() {
 	const Library = require('./lib/models/library.js');
-	const format = require('./lib/format.js');
+	const app = Express();
+	/** @type {Number} */
+	const port = process.env['PORT'] || 3000;
+	// requires dependency injection
+	const log = config.provider.log;
 
 	log.error('Restarting %s application', (config.isProduction) ? 'production' : 'development');
 
+	defineViews(app);
+	applyMiddleware(app);
+
+	Library.load(() => {
+		// library must be loaded before routes are defined
+		defineRoutes();
+		app.listen(port);
+		//https.createServer(options, app).listen(port);
+		log.info('Listening on port %d', port);
+	});
+}
+
+/**
+ * @see https://github.com/donpark/hbs/blob/master/examples/extend/app.js
+ * @see https://npmjs.org/package/express-hbs
+ * @see http://mustache.github.com/mustache.5.html
+ */
+function defineViews(app) {
+	/** @type {ExpressHbs} */
+	const hbs = require('express-hbs');
+	const format = require('./lib/format.js');
+	const template = require('./lib/template.js');
+	const engine = 'hbs';
+
 	// http://expressjs.com/4x/api.html#app-settings
 	app.set('views', __dirname + '/views');
-	/**
-	 * Mustache templating
-	 * @see http://mustache.github.com/mustache.5.html
-	 */
-	app.set('view engine', 'hbs');
-	app.engine('hbs', hbs.express4({
+	app.set('view engine', engine);
+	app.engine(engine, hbs.express4({
 		defaultLayout: __dirname + '/views/' + template.layout.main + '.hbs',
 		partialsDir: __dirname + '/views/partials'
 	}));
 
+	// formatting methods for the views
 	for (let name in format.helpers) {
 		hbs.registerHelper(name, format.helpers[name]);
 	}
+}
+
+/**
+ * @param app
+ */
+function applyMiddleware(app) {
+	const compress = require('compression');
+	const bodyParser = require('body-parser');
+	const cookies = require('cookies');
+	const wwwhisper = require('connect-wwwhisper');
+	const outputCache = require('./lib/output-cache.js');
 
 	app.use(filter(/^\/(admin|wwwhisper)(?!.*(delete|load)$)/, wwwhisper(false)));
 	app.use(cookies.express([config.flickr.userID, config.facebook.adminID]));
@@ -79,13 +75,6 @@ function configure() {
 	app.use(compress());
 	app.use(outputCache());
 	app.use(Express.static(__dirname + '/dist'));
-
-	Library.load(() => {
-		defineRoutes();
-		app.listen(port);
-		//https.createServer(options, app).listen(port);
-		log.info('Listening on port %d', port);
-	});
 }
 
 /**
@@ -96,6 +85,38 @@ function configure() {
  */
 function filter(regex, fn) {
 	return (req, res, next) => { if (regex.test(req.path)) { fn(req, res, next); } else { next(); }}
+}
+
+/**
+ * Inject provider dependencies
+ */
+function injectDependencies() {
+	const RedisProvider = require('./lib/providers/redis-cache.js');
+	const FlickrProvider = require('./lib/providers/flickr-lib.js');
+	let redisHost = config.env('REDISCLOUD_URL');
+
+	config.provider.cache = new RedisProvider(redisHost);
+	config.provider.library = new FlickrProvider({
+		key: env('FLICKR_KEY'),
+		userID: '60950751@N04',
+		appID: '72157631007435048',
+		secret: env('FLICKR_SECRET'),
+		token: env('FLICKR_TOKEN'),
+		tokenSecret: env('FLICKR_TOKEN_SECRET'),
+		defaultCollection: '72157630885395608',
+		photoSet: {
+			featured: '72157631638576162',
+			poetry: '72157632729508554'
+		}
+	});
+
+	if (config.isProduction) {
+		const RedisLog = require('./lib/providers/redis-log.js');
+		config.provider.log = new RedisLog(redisHost);
+	} else {
+		const ConsoleLog = require('./lib/providers/console-log.js');
+		config.provider.log = new ConsoleLog();
+	}
 }
 
 /**
