9e9ff569b6f7bbc01c7cbba2403175e805d5f7a2
Jason Abbott <abbottj@slhs.org>
2015-11-23 19:27:25 +0000

    pdf checkpoint

--- test/providers/google/google-file.test.js
+++ test/providers/google/google-file.test.js
@@ -4,12 +4,20 @@ const mocha = require('mocha');
 const expect = require('chai').expect;
 const google = require('../../../lib/providers/google/google-file.js');
 
-describe('Google GPX Provider', ()=> {
+describe('Google File Provider', ()=> {
 	it.skip('retrieves GPX file from a Google Drive folder', ()=> {
 
 	});
 
 	it.skip('converts a GPX file to GeoJSON', ()=> {
+
+	});
+
+	it.skip('retrieves PDF file from a Google Drive folder', ()=> {
+
+	});
+
+	it.skip('writes generated PDF to folder', ()=> {
 
 	});
 });
