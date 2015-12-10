11f3a608a47402ccd9591b7501e7acda2d8a79c6
Jason Abbott <abbottj@slhs.org>
2015-12-10 00:23:26 +0000

    fix broken test

--- test/mock-index.js
+++ test/mock-index.js
@@ -3,7 +3,7 @@
 const lib = require('../lib/index.js');
 const NullLog = lib.Log.Null;
 
-app.provider.log = new NullLog();
+lib.provider.log = new NullLog();
 
 /** @type {LibraryIndex} */
 module.exports = lib;
