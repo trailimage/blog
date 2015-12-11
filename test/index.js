e674e733ef25a9de678219ecbd095975b549c59a
Jason Abbott <abbottj@slhs.org>
2015-12-11 19:12:40 +0000

    namespace work

--- test/index.js
+++ test/index.js
@@ -1,23 +1,22 @@
 'use strict';
 
-/** @type {RootNamespace} */
 const TI = require('../lib');
 
 TI.active.log = new TI.Provider.Log.Null();
 
 /**
- *
- * @property {MockRequest} Request
- * @property {MockResponse} Response
+ * @namespace TI.Mock
  */
 TI.Mock = class {
 	/**
+	 * @namespace TI.Mock.Request
 	 * @returns {MockRequest}
 	 * @constructor
 	 */
 	static get Request() { return require('./mock-request.js'); }
 
 	/**
+	 * @namespace TI.Mock.Response
 	 * @returns {MockResponse}
 	 * @constructor
 	 */
