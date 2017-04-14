50fa2e2e3e934ae34e21a4f98f45371307984a50
Jason Abbott <dev@trailimage.com>
2017-04-14 17:15:54 -0600

    map styling

--- src/js/mapbox.js
+++ src/js/mapbox.js
@@ -4,14 +4,32 @@ var nav = new mapboxgl.NavigationControl();
 
 var map = new mapboxgl.Map({
     container: 'map-canvas',
-    style: 'mapbox://styles/mapbox/satellite-streets-v9',
-    center: [-116.215019, 43.618881],
-    zoom: 8
-});
+    //style: 'mapbox://styles/mapbox/satellite-streets-v9',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-116.0987, 44.9110],
+    zoom: 6.5
+}); 
 
-map.addControl(nav, 'top-left');
+map.addControl(nav, 'top-right');
 
 map.on('load', function() {
+   var showSatellite = false;
+   var style = {
+      street: 'streets-v9',
+      satellite: 'satellite-streets-v9'
+   };
+   var $button = $('#toggle-satellite');
+
+   // https://bl.ocks.org/tristen/0c0ed34e210a04e89984
+   $button.click(function() {
+      showSatellite = !showSatellite; 
+      map.once('data', function(e) {
+         if (e.dataType == 'style') { addMapLayers(); }
+      });
+      map.setStyle('mapbox://styles/mapbox/' + (showSatellite ? style.satellite : style.street));
+      addMapLayers();
+   });
+
    map.addSource('photos', {
       type: 'geojson',
       data: 'http://localhost:3000/geo.json',
@@ -20,6 +38,10 @@ map.on('load', function() {
       clusterRadius: 20
    });
 
+   addMapLayers();
+});
+
+function addMapLayers() {
    // https://www.mapbox.com/mapbox-gl-js/style-spec/#layers-circle
    // https://www.mapbox.com/mapbox-gl-js/style-spec/#types-function
    map.addLayer({
@@ -28,29 +50,23 @@ map.on('load', function() {
       source: 'photos',
       filter: ['has', 'point_count'],
       paint: {
-         'circle-color': {
-            property: 'point_count',
-            type: 'interval',
-            stops: [
-               [0, "#51bbd6"],
-               [100, "#f1f075"],
-               [750, "#747e73"],
-            ]
-         },
+         'circle-color': '#524948',
          'circle-radius': {
-            property: 'point_count',
+            property: 'point_count', 
             type: 'interval',
             stops: [
-               [0, 20],
-               [100, 30],
-               [750, 40]
+               [0, 13],
+               [50, 17],
+               [100, 20]
             ]
          },
+         'circle-opacity': 0.6,
          'circle-stroke-width': 3,
-         'circle-stroke-color': '#524948' 
+         'circle-stroke-color': '#ccc' 
       }
    });
 
+   // https://www.mapbox.com/mapbox-gl-js/style-spec/#layers-symbol
    map.addLayer({
       id: 'cluster-count',
       type: 'symbol',
@@ -58,13 +74,15 @@ map.on('load', function() {
       filter: ['has', 'point_count'],
       layout: {
          'text-field': '{point_count_abbreviated}',
-         'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-         'text-size': 12
+         'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
+         'text-size': 14
+      },
+      paint: {
+         'text-color': '#fff'
       }
    });
 
    // https://www.mapbox.com/mapbox-gl-js/example/custom-marker-icons/
-
    map.addLayer({
       id: 'unclustered-point',
       type: 'circle',
@@ -77,4 +95,4 @@ map.on('load', function() {
          'circle-stroke-color': '#fff' 
       }
    });
-})
+}
