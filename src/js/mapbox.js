1bfbf244b26ca01de1cb91cfcb8c45355d1b2687
Jason Abbott <dev@trailimage.com>
2017-04-13 22:56:31 -0600

    styling

--- src/js/mapbox.js
+++ src/js/mapbox.js
@@ -1,25 +1,17 @@
 'use strict';
 
+var nav = new mapboxgl.NavigationControl();
+
 var map = new mapboxgl.Map({
     container: 'map-canvas',
-    style: 'mapbox://styles/mapbox/satellite-streets-v9'
+    style: 'mapbox://styles/mapbox/satellite-streets-v9',
+    center: [-116.215019, 43.618881],
+    zoom: 8
 });
 
+map.addControl(nav, 'top-left');
+
 map.on('load', function() {
-   // map.addLayer({
-   //    id: 'photos',
-   //    type: 'symbol',
-   //    source: {
-   //       type: 'geojson',
-   //       data: 'http://localhost:3000/geo.json'
-   //    },
-   //    layout: {
-   //       "text-field": "{title}",
-   //       "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-   //       "text-offset": [0, 0.6],
-   //       "text-anchor": "top"
-   //    }
-   // });
    map.addSource('photos', {
       type: 'geojson',
       data: 'http://localhost:3000/geo.json',
@@ -28,55 +20,61 @@ map.on('load', function() {
       clusterRadius: 20
    });
 
+   // https://www.mapbox.com/mapbox-gl-js/style-spec/#layers-circle
+   // https://www.mapbox.com/mapbox-gl-js/style-spec/#types-function
    map.addLayer({
-        id: 'clusters',
-        type: 'circle',
-        source: 'photos',
-        filter: ['has', 'point_count'],
-        paint: {
-            'circle-color': {
-                property: "point_count",
-                type: 'interval',
-                stops: [
-                    [0, "#51bbd6"],
-                    [100, "#f1f075"],
-                    [750, "#f28cb1"],
-                ]
-            },
-            'circle-radius': {
-                property: "point_count",
-                type: "interval",
-                stops: [
-                    [0, 20],
-                    [100, 30],
-                    [750, 40]
-                ]
-            }
-        }
-    });
+      id: 'clusters',
+      type: 'circle',
+      source: 'photos',
+      filter: ['has', 'point_count'],
+      paint: {
+         'circle-color': {
+            property: 'point_count',
+            type: 'interval',
+            stops: [
+               [0, "#51bbd6"],
+               [100, "#f1f075"],
+               [750, "#747e73"],
+            ]
+         },
+         'circle-radius': {
+            property: 'point_count',
+            type: 'interval',
+            stops: [
+               [0, 20],
+               [100, 30],
+               [750, 40]
+            ]
+         },
+         'circle-stroke-width': 3,
+         'circle-stroke-color': '#524948' 
+      }
+   });
 
-    map.addLayer({
-        id: 'cluster-count',
-        type: 'symbol',
-        source: 'photos',
-        filter: ['has', 'point_count'],
-        layout: {
-            'text-field': '{point_count_abbreviated}',
-            'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-            'text-size': 12
-        }
-    });
+   map.addLayer({
+      id: 'cluster-count',
+      type: 'symbol',
+      source: 'photos',
+      filter: ['has', 'point_count'],
+      layout: {
+         'text-field': '{point_count_abbreviated}',
+         'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+         'text-size': 12
+      }
+   });
 
-    map.addLayer({
-        id: 'unclustered-point',
-        type: 'circle',
-        source: 'photos',
-        filter: ["!has", "point_count"],
-        paint: {
-            'circle-color': '#11b4da',
-            'circle-radius': 4,
-            'circle-stroke-width': 1,
-            'circle-stroke-color': '#fff' 
-        }
-    });
+   // https://www.mapbox.com/mapbox-gl-js/example/custom-marker-icons/
+
+   map.addLayer({
+      id: 'unclustered-point',
+      type: 'circle',
+      source: 'photos',
+      filter: ['!has', 'point_count'],
+      paint: {
+         'circle-color': '#11b4da',
+         'circle-radius': 4,
+         'circle-stroke-width': 1,
+         'circle-stroke-color': '#fff' 
+      }
+   });
 })
