2896040d6d46c9ba67967cb44d2c3d5ca58fa425
Jason Abbott <dev@trailimage.com>
2017-04-26 18:33:13 -0600

    fix is

--- src/is.ts
+++ src/is.ts
@@ -59,7 +59,7 @@ function callable(v:any):v is Function {
 const bigInt = (n:any) => integer(n) && (n < -32768 || n > 32767);
 
 function cacheItem(o:any):o is Cache.Item {
-   return (value(o) && o['buffer'] && o['eTag'])
+   return (value<Object>(o) && o['buffer'] && o['eTag']);
 }
 
 /** Whether value exists and is an array */
