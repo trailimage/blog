37dbbdafa9932a92915edd6599898bcf722e5feb
Jason Abbott <dev@trailimage.com>
2018-03-21 18:22:06 -0600

    fix model ref

--- src/factory/index.ts
+++ src/factory/index.ts
@@ -1,4 +1,5 @@
 export { make as makeCategory } from './category';
 export { make as makePhoto } from './photo';
+export { make as makePost } from './post';
 export { make as makePhotoSize } from './photo-size';
 export { make as makeVideoInfo } from './video-info';
